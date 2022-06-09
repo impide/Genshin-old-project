54024d1ed2523489ea75f148e63fc09a765963df
Nathanael421 <nathanael.rayapin@gmail.com>
2022-06-09 18:15:25 +0200

    Setup the routes, main view and modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageHomeComponent } from './views/main-page-home/main-page-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'main-page-home', pathMatch: 'full'},
+  {path: 'main-page-home', component: MainPageHomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavbarComponent } from './components/main-navbar/main-navbar.component';
+import { MainPageHomeComponent } from './views/main-page-home/main-page-home.component';
+import { MainFooterComponent } from './components/main-footer/main-footer.component';
+import { MainContactComponent } from './components/main-contact/main-contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavbarComponent,
+    MainPageHomeComponent,
+    MainFooterComponent,
+    MainContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
