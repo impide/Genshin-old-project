0d24b4019a8ebc144265655e22a355b998c8a0ea
Nathanael421 <nathanael.rayapin@gmail.com>
2022-06-14 11:13:06 +0200

    main page component

--- src/app/views/main-page-home/main-page-home.component.ts
+++ src/app/views/main-page-home/main-page-home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AppStore } from 'src/app/core/data/app-store';
+import { LegalNotice } from 'src/app/core/data/legal-notice';
+import { LocationsDesign } from 'src/app/core/data/location-design';
 import { NewsUpdate } from 'src/app/core/data/new-update';
+import { SocialLists } from 'src/app/core/data/social-media';
 
 @Component({
   selector: 'app-main-page-home',
@@ -8,8 +11,20 @@ import { NewsUpdate } from 'src/app/core/data/new-update';
   styleUrls: ['./main-page-home.component.scss']
 })
 export class MainPageHomeComponent implements OnInit {
+  // Games Store 
   stores: AppStore[] = AppStore;
+
+  // News about Games
   newsUpdate: NewsUpdate[] = NewsUpdate;
+
+  // Location Design
+  locationsDesign: LocationsDesign[] = LocationsDesign;
+
+  // Social Media
+  socialLists: SocialLists[] = SocialLists;
+
+  // Information Legal
+  legalNotice: LegalNotice[] = LegalNotice;
   
   constructor() {}
 
