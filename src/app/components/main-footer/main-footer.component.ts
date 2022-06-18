65603652f17bd01a585b651cff793e71dd225d28
Nathanael421 <nathanael.rayapin@gmail.com>
2022-06-18 23:04:19 +0200

    main footer component

--- src/app/components/main-footer/main-footer.component.ts
+++ src/app/components/main-footer/main-footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LegalNotice } from 'src/app/core/data/legal-notice';
+import { SocialLists } from 'src/app/core/data/social-media';
 
 @Component({
   selector: 'app-main-footer',
@@ -6,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-footer.component.scss']
 })
 export class MainFooterComponent implements OnInit {
+  // Social Media
+  socialLists: SocialLists[] = SocialLists;
+
+  // Information Legal
+  legalNotice: LegalNotice[] = LegalNotice;
 
   constructor() { }
 
