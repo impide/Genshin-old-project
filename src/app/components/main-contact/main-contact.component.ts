d0177a6480080b428d50949ea6c57a357a4a055f
Nathanael421 <nathanael.rayapin@gmail.com>
2022-06-18 23:04:01 +0200

    contact component

--- src/app/components/main-contact/main-contact.component.ts
+++ src/app/components/main-contact/main-contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SocialLists } from 'src/app/core/data/social-media';
 
 @Component({
   selector: 'app-main-contact',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-contact.component.scss']
 })
 export class MainContactComponent implements OnInit {
-
+  // Social Media
+  socialLists: SocialLists[] = SocialLists;
+  
   constructor() { }
 
   ngOnInit(): void {
