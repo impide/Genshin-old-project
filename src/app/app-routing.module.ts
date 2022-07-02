0bbfa79ccec0b74e6437c7183ef11848afbf98ee
Nathanael421 <nathanael.rayapin@gmail.com>
2022-07-02 22:33:09 +0200

    New routes (characters section)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CharactersComponent } from './views/characters/characters.component';
+import { ComingSoonComponent } from './views/characters/coming-soon/coming-soon.component';
+import { InazumaComponent } from './views/characters/inazuma/inazuma.component';
+import { LiyueComponent } from './views/characters/liyue/liyue.component';
+import { MondstadtComponent } from './views/characters/mondstadt/mondstadt.component';
 import { CurrentEventsComponent } from './views/current-events/current-events.component';
 import { NewsComponent } from './views/current-events/news/news.component';
 import { MainPageHomeComponent } from './views/main-page-home/main-page-home.component';
@@ -11,7 +15,12 @@ const routes: Routes = [
   {path: 'current-events', component: CurrentEventsComponent, children: [
     {path: 'news', component: NewsComponent}
   ]},
-  {path: 'characters', component: CharactersComponent}
+  {path: 'characters', component: CharactersComponent, children: [
+    {path: 'mondstadt', component: MondstadtComponent},
+    {path: 'liyue', component: LiyueComponent},
+    {path: 'inazuma', component: InazumaComponent},
+    {path: 'coming-soon', component: ComingSoonComponent},
+  ]}
 ];
 
 @NgModule({
