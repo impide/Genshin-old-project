820c5a9cbb2af8a299c709158afe18ee2c95bcd5
Nathanael421 <nathanael.rayapin@gmail.com>
2022-07-05 18:26:03 +0200

    faq questions

---
+++ src/app/core/data/faq-question.ts
@@ -0,0 +1,43 @@
+export const FaqQuestions: FaqData[] = [
+    {
+        question: 'Que faire en cas d\'échec de téléchargement/de mis...',
+        path: ''
+    },
+    {
+        question: 'Le jeu ne démarre pas ou ne répond pas quand je l...',
+        path: ''
+    },
+    {
+        question: 'Pourquoi le jeu plante-il subitement',
+        path: ''
+    },
+    {
+        question: 'Est-ce que la version Pc de Genshin Impact fonctio...',
+        path: ''
+    },
+    {
+        question: 'Quelles sont les langues disponibles dans la versio...',
+        path: ''
+    },
+    {
+        question: 'Pendant le téléchargement, je reçois un message m...',
+        path: ''
+    },
+    {
+        question: 'Après le démarrage du jeu, l\'écran se fige avant l\'ap...',
+        path: ''
+    },
+    {
+        question: 'Que faire en cas de ralentissement du jeu ?',
+        path: ''
+    },
+    {
+        question: 'Genshin Impact peut-il être téléchargé gratuitement ...',
+        path: ''
+    },
+]
+
+export interface FaqData {
+    question: string;
+    path: string;
+}
